337decaa3a66d316d3af723ce08f5351cbbe7719
TongQuangThanh <tongquangthanh94@gmail.com>
2022-12-24 16:48:33 +0700

    fix lint

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "Danghoangthienp4-env.eba-63hawkka.ap-northeast-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'Danghoangthienp4-env.eba-63hawkka.ap-northeast-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
